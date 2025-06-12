4db963a8a0054b71fec6f28dc98e5c90ce1162e0
wavesj <wavesj04@gmail.com>
2025-06-11 17:54:48 -0700

    Implement computer and human choice function

--- javascript.js
+++ javascript.js
@@ -1,1 +1,15 @@
-console.log("Hello World")
+function getComputerChoice() {
+    let choice = Math.floor(Math.random()*3);
+    if (choice < 1) {
+        return "rock";
+    } else if (choice < 2) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
+
+function getHumanChoice() {
+    let input = prompt("Please choose rock, paper, or scissors: ");
+    return input;
+}
